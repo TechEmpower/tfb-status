651e071f9c942cf1ee593532637e241d2a96588f
Michael Hixson <mhixson@techempower.com>
2018-02-21 10:29:36 -0800

    Add JSON view of the results detail page

--- src/main/java/tfb/status/handler/DetailPageHandler.java
+++ src/main/java/tfb/status/handler/DetailPageHandler.java
@@ -1,13 +1,17 @@
 package tfb.status.handler;
 
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
 import static com.google.common.net.MediaType.HTML_UTF_8;
+import static com.google.common.net.MediaType.JSON_UTF_8;
 import static io.undertow.util.Headers.CONTENT_TYPE;
 import static io.undertow.util.Methods.GET;
 import static io.undertow.util.StatusCodes.NOT_FOUND;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.DisableCacheHandler;
+import io.undertow.server.handlers.SetHeaderHandler;
 import java.util.Objects;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -26,13 +30,16 @@ public final class DetailPageHandler implements HttpHandler {
 
   @Inject
   public DetailPageHandler(HomeResultsReader homeResultsReader,
-                           MustacheRenderer mustacheRenderer) {
+                           MustacheRenderer mustacheRenderer,
+                           ObjectMapper objectMapper) {
 
     HttpHandler handler = new CoreHandler(homeResultsReader,
-                                          mustacheRenderer);
+                                          mustacheRenderer,
+                                          objectMapper);
 
     handler = new MethodHandler().addMethod(GET, handler);
     handler = new DisableCacheHandler(handler);
+    handler = new SetHeaderHandler(handler, ACCESS_CONTROL_ALLOW_ORIGIN, "*");
 
     delegate = handler;
   }
@@ -43,19 +50,26 @@ public final class DetailPageHandler implements HttpHandler {
   }
 
   private static final class CoreHandler implements HttpHandler {
-    private final MustacheRenderer mustacheRenderer;
     private final HomeResultsReader homeResultsReader;
+    private final MustacheRenderer mustacheRenderer;
+    private final ObjectMapper objectMapper;
 
     CoreHandler(HomeResultsReader homeResultsReader,
-                MustacheRenderer mustacheRenderer) {
+                MustacheRenderer mustacheRenderer,
+                ObjectMapper objectMapper) {
       this.homeResultsReader = Objects.requireNonNull(homeResultsReader);
       this.mustacheRenderer = Objects.requireNonNull(mustacheRenderer);
+      this.objectMapper = Objects.requireNonNull(objectMapper);
     }
 
     @Override
-    public void handleRequest(HttpServerExchange exchange) {
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
       String uuid = exchange.getRelativePath()
                             .substring(1); // omit leading slash
+
+      boolean isJson = uuid.endsWith(".json");
+      if (isJson)
+        uuid = uuid.substring(0, uuid.length() - ".json".length());
 
       ResultsView result = homeResultsReader.resultsByUuid(uuid);
 
@@ -65,10 +79,16 @@ public final class DetailPageHandler implements HttpHandler {
       }
 
       DetailPageView view = new DetailPageView(result);
-      String html = mustacheRenderer.render("detail.mustache", view);
 
-      exchange.getResponseHeaders().put(CONTENT_TYPE, HTML_UTF_8.toString());
-      exchange.getResponseSender().send(html);
+      if (isJson) {
+        String json = objectMapper.writeValueAsString(view);
+        exchange.getResponseHeaders().put(CONTENT_TYPE, JSON_UTF_8.toString());
+        exchange.getResponseSender().send(json);
+      } else {
+        String html = mustacheRenderer.render("detail.mustache", view);
+        exchange.getResponseHeaders().put(CONTENT_TYPE, HTML_UTF_8.toString());
+        exchange.getResponseSender().send(html);
+      }
     }
   }
 }
