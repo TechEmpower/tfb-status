be945a7651d93d33d8589fc60683d7b8bb294f89
Michael Hixson <mhixson@techempower.com>
2018-02-22 09:40:29 -0800

    Remove an unused import

--- src/main/java/tfb/status/handler/UploadResultsHandler.java
+++ src/main/java/tfb/status/handler/UploadResultsHandler.java
@@ -31,7 +31,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
