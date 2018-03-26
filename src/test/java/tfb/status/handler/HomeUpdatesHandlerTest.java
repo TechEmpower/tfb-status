d4c252f2ec4b38ce9b66c4cf6553d6f64cee2453
Michael Hixson <mhixson@techempower.com>
2018-03-26 11:15:53 -0700

    Fix HomeUpdatesHandlerTest

--- src/test/java/tfb/status/handler/HomeUpdatesHandlerTest.java
+++ src/test/java/tfb/status/handler/HomeUpdatesHandlerTest.java
@@ -1,20 +1,10 @@
 package tfb.status.handler;
 
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
-import static io.undertow.util.StatusCodes.OK;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tfb.status.util.MoreAssertions.assertContains;
-import static tfb.status.util.MoreAssertions.assertMediaType;
 
-import com.google.common.net.MediaType;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import javax.ws.rs.core.Response;
-import org.glassfish.jersey.media.sse.EventInput;
-import org.glassfish.jersey.media.sse.InboundEvent;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.glassfish.jersey.media.sse.EventSource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -44,47 +34,33 @@ public final class HomeUpdatesHandlerTest {
    */
   @Test
   public void testGet() {
-    try (Response response =
-             services.httpClient()
-                     .target(services.localUri("/updates"))
-                     .request()
-                     .get()) {
+    EventSource eventSource =
+        EventSource.target(services.httpClient()
+                                   .target(services.localUri("/updates")))
+                   .build();
 
-      assertEquals(OK, response.getStatus());
+    AtomicBoolean receivedEvent = new AtomicBoolean(false);
 
-      assertMediaType(
-          MediaType.create("text", "event-stream"),
-          response.getHeaderString(CONTENT_TYPE));
+    eventSource.register(
+        event -> {
 
-      ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-      try (EventInput eventInput = response.readEntity(EventInput.class)) {
+          assertContains(
+              "03da6340-d56c-4584-9ef2-702106203809",
+              event.readData());
 
-        updates.sendUpdate("03da6340-d56c-4584-9ef2-702106203809");
+          receivedEvent.set(true);
 
-        // The call to eventInput.read() has the potential to block the current
-        // thread forever.  We schedule a timeout to ensure that won't happen.
-        ScheduledFuture<?> timeout =
-            scheduler.schedule(eventInput::close, 5, SECONDS);
+        });
 
-        InboundEvent event = eventInput.read();
-
-        // If we made it this far, we don't need the timeout anymore.
-        timeout.cancel(false);
-
-        // FIXME: This assertion fails sometimes.  Why?
-        assertNotNull(
-            event,
-            "The server should send an event before the timeout");
-
-        String data = event.readData();
-
-        assertContains(
-            "03da6340-d56c-4584-9ef2-702106203809",
-            event.readData());
-
-      } finally {
-        scheduler.shutdownNow();
-      }
+    eventSource.open();
+    try {
+      updates.sendUpdate("03da6340-d56c-4584-9ef2-702106203809");
+    } finally {
+      eventSource.close();
     }
+
+    assertTrue(
+        receivedEvent.get(),
+        "The SSE client should have received an event from the server");
   }
 }
