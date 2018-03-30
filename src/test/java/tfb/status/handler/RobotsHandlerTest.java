06e84ffe4b1da49634a26eb6e0ab912d44bf3841
Michael Hixson <mhixson@techempower.com>
2018-03-30 15:02:02 -0700

    Improve RobotsHandlerTest to not quit on first failed url test

--- src/test/java/tfb/status/handler/RobotsHandlerTest.java
+++ src/test/java/tfb/status/handler/RobotsHandlerTest.java
@@ -3,13 +3,14 @@ package tfb.status.handler;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static com.google.common.net.MediaType.PLAIN_TEXT_UTF_8;
 import static io.undertow.util.StatusCodes.OK;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static tfb.status.util.MoreAssertions.assertMediaType;
 
 import crawlercommons.robots.BaseRobotRules;
 import crawlercommons.robots.SimpleRobotRulesParser;
-import java.util.List;
+import java.util.stream.Stream;
 import javax.ws.rs.core.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -52,24 +53,26 @@ public final class RobotsHandlerTest {
 
       byte[] responseBytes = response.readEntity(byte[].class);
 
-      SimpleRobotRulesParser robotRulesParser = new SimpleRobotRulesParser();
-
       BaseRobotRules robotRules =
-          robotRulesParser.parseContent(
+          new SimpleRobotRulesParser().parseContent(
               /* url= */ services.localUri("/robots.txt").toString(),
               /* content = */ responseBytes,
               /* contentType= */ response.getHeaderString(CONTENT_TYPE),
               /* robotNames=*/ "Googlebot");
 
-      for (String path : List.of("/",
-                                 "/about",
-                                 "/raw/results.2017-12-26-05-07-14-321.json",
-                                 "/unzip/results.2017-12-29-23-04-02-541.zip/gemini/out.txt")) {
-        String url = services.localUri(path).toString();
-        assertFalse(
-            robotRules.isAllowed(url),
-            "should be disallowed: " + url);
-      }
+      assertAll(
+          Stream.of("/",
+                    "/about",
+                    "/raw/results.2017-12-26-05-07-14-321.json",
+                    "/unzip/results.2017-12-29-23-04-02-541.zip/gemini/out.txt")
+                .map(path -> services.localUri(path).toString())
+                .map(url -> () -> assertDisallowed(robotRules, url)));
     }
+  }
+
+  private static void assertDisallowed(BaseRobotRules robotRules, String url) {
+    assertFalse(
+        robotRules.isAllowed(url),
+        "should be disallowed: " + url);
   }
 }
