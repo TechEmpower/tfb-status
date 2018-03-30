e36bd8f30ae3de722a6af7af4081644725b0ea49
Michael Hixson <mhixson@techempower.com>
2018-03-30 16:43:11 -0700

    Add tests for RequestValues.queryParameter

--- src/test/java/tfb/status/undertow/extensions/RequestValuesTest.java
+++ src/test/java/tfb/status/undertow/extensions/RequestValuesTest.java
@@ -1,6 +1,7 @@
 package tfb.status.undertow.extensions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import io.undertow.server.HttpServerExchange;
 import java.util.ArrayDeque;
@@ -13,8 +14,61 @@ public final class RequestValuesTest {
   private static final String PARAMETER_NAME = "foo";
   private static final int VALUE_IF_ABSENT = -11;
   private static final int VALUE_IF_MALFORMED = 62;
-  private static final int EXPECTED_VALUE = 7;
+  private static final int EXPECTED_VALUE_AS_INT = 7;
+  private static final String EXPECTED_VALUE_AS_STRING = "7";
   private static final String UNPARSEABLE_VALUE = "ten";
+
+  /**
+   * Tests that {@link RequestValues#queryParameter(HttpServerExchange, String)}
+   * reads a present value from the query string.
+   */
+  @Test
+  public void testQueryParameter_happyPath() {
+    var exchange = new HttpServerExchange(null);
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
+    assertEquals(
+        EXPECTED_VALUE_AS_STRING,
+        RequestValues.queryParameter(exchange, PARAMETER_NAME));
+  }
+
+  /**
+   * Tests that {@link RequestValues#queryParameter(HttpServerExchange, String)}
+   * returns {@code null} when there is no query parameter with the given name.
+   */
+  @Test
+  public void testQueryParameter_nullWhenNoValue() {
+    var exchange = new HttpServerExchange(null);
+    assertNull(RequestValues.queryParameter(exchange, PARAMETER_NAME));
+  }
+
+  /**
+   * Tests that {@link RequestValues#queryParameter(HttpServerExchange, String)}
+   * returns {@code null} when the exchange has an empty collection of values
+   * for the query parameter with the given name.
+   *
+   * <p>This is a slightly different scenario than when there is no value for
+   * the parameter at all.  It may be that the only way to reach this state is
+   * to manipulate the query parameter value collection directly.
+   */
+  @Test
+  public void testQueryParameter_nullWhenEmptyValueCollection() {
+    var exchange = new HttpServerExchange(null);
+    exchange.getQueryParameters().put(PARAMETER_NAME, new ArrayDeque<>());
+    assertNull(RequestValues.queryParameter(exchange, PARAMETER_NAME));
+  }
+
+  /**
+   * Tests that {@link RequestValues#queryParameter(HttpServerExchange, String)}
+   * returns {@code null} when there are two values for the given query
+   * parameter.
+   */
+  @Test
+  public void testQueryParameter_nullWhenTwoValues() {
+    var exchange = new HttpServerExchange(null);
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
+    assertNull(RequestValues.queryParameter(exchange, PARAMETER_NAME));
+  }
 
   /**
    * Tests that {@link
@@ -24,9 +78,9 @@ public final class RequestValuesTest {
   @Test
   public void testQueryParameterAsInt_happyPath() {
     var exchange = new HttpServerExchange(null);
-    exchange.addQueryParam(PARAMETER_NAME, String.valueOf(EXPECTED_VALUE));
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
     assertEquals(
-        EXPECTED_VALUE,
+        EXPECTED_VALUE_AS_INT,
         RequestValues.queryParameterAsInt(
             exchange, PARAMETER_NAME, VALUE_IF_ABSENT, VALUE_IF_MALFORMED));
   }
@@ -75,8 +129,8 @@ public final class RequestValuesTest {
   @Test
   public void testQueryParameterAsInt_defaultWhenTwoValues() {
     var exchange = new HttpServerExchange(null);
-    exchange.addQueryParam(PARAMETER_NAME, String.valueOf(EXPECTED_VALUE));
-    exchange.addQueryParam(PARAMETER_NAME, String.valueOf(EXPECTED_VALUE));
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
+    exchange.addQueryParam(PARAMETER_NAME, EXPECTED_VALUE_AS_STRING);
     assertEquals(
         VALUE_IF_MALFORMED,
         RequestValues.queryParameterAsInt(
