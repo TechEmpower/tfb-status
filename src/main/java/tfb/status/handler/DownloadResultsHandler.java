b47c036207e96a7be58ed9a322e496f488a659b9
Michael Hixson <mhixson@techempower.com>
2018-11-10 19:39:38 -0800

    Make DownloadResultsHandler use MethodHandler like everyone else

--- src/main/java/tfb/status/handler/DownloadResultsHandler.java
+++ src/main/java/tfb/status/handler/DownloadResultsHandler.java
@@ -1,6 +1,7 @@
 package tfb.status.handler;
 
 import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static io.undertow.util.Methods.GET;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
@@ -10,6 +11,7 @@ import io.undertow.server.handlers.resource.ResourceHandler;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import tfb.status.service.FileStore;
+import tfb.status.undertow.extensions.MethodHandler;
 
 /**
  * Handles requests to download full, raw, previously-uploaded results files.
@@ -23,6 +25,7 @@ public final class DownloadResultsHandler implements HttpHandler {
     var resourceManager = new PathResourceManager(fileStore.resultsDirectory());
 
     HttpHandler handler = new ResourceHandler(resourceManager);
+    handler = new MethodHandler().addMethod(GET, handler);
     handler = new SetHeaderHandler(handler, ACCESS_CONTROL_ALLOW_ORIGIN, "*");
 
     delegate = handler;
