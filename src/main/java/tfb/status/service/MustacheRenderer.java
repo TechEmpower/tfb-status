06e00791e6d08a546afbb0a90b1dbb9ebb07aebe
Michael Hixson <mhixson@techempower.com>
2018-11-15 09:28:52 -0800

    Avoid using java.io.File with Mustache

--- src/main/java/tfb/status/service/MustacheRenderer.java
+++ src/main/java/tfb/status/service/MustacheRenderer.java
@@ -2,12 +2,18 @@ package tfb.status.service;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheException;
 import com.github.mustachejava.MustacheNotFoundException;
+import com.github.mustachejava.MustacheResolver;
 import com.github.mustachejava.resolver.ClasspathResolver;
-import com.github.mustachejava.resolver.FileSystemResolver;
+import java.io.IOException;
+import java.io.Reader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.Objects;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import tfb.status.config.MustacheConfig;
@@ -69,13 +75,51 @@ public final class MustacheRenderer {
       }
       case FILE_SYSTEM: {
         Path mustacheRoot = Path.of(config.root);
-        var resolver = new FileSystemResolver(mustacheRoot.toFile());
+        var resolver = new NioFileSystemResolver(mustacheRoot);
         return fileName -> {
           var newFactory = new DefaultMustacheFactory(resolver);
           return newFactory.compile(fileName);
         };
       }
     }
+
     throw new AssertionError("Unknown resource mode: " + config.mode);
+  }
+
+  // Avoid using java.io.File or Path.toFile().  These are incompatible with
+  // jimfs, the library that provides an in-memory file system that we use for
+  // testing.
+  private static final class NioFileSystemResolver implements MustacheResolver {
+    private final Path root;
+
+    NioFileSystemResolver(Path root) {
+      this.root = Objects.requireNonNull(root);
+    }
+
+    @Override
+    @Nullable
+    public Reader getReader(String resourceName) {
+      Path file;
+      try {
+        file = root.resolve(resourceName);
+      } catch (InvalidPathException e) {
+        throw new MustacheException("Invalid file path", e);
+      }
+
+      if (!file.normalize().equals(file))
+        throw new MustacheException("File path must be normalized");
+
+      if (!file.startsWith(root))
+        throw new MustacheException("File path must start with the root path");
+
+      if (!Files.isRegularFile(file))
+        return null;
+
+      try {
+        return Files.newBufferedReader(file);
+      } catch (IOException e) {
+        throw new MustacheException("Could not open file", e);
+      }
+    }
   }
 }
