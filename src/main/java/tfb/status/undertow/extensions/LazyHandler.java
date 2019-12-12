fec5cc5346fdb2859ac6ce41bfeebe2d8b000f36
Michael Hixson <mhixson@techempower.com>
2019-12-12 15:14:56 -0800

    Use Supplier instead of Provider
    It used to be the case that we were passing Provider instances here that
    HK2 gave to us, but that is no longer the case.  So we should prefer the
    more general functional interface.

--- src/main/java/tfb/status/undertow/extensions/LazyHandler.java
+++ src/main/java/tfb/status/undertow/extensions/LazyHandler.java
@@ -3,22 +3,22 @@ package tfb.status.undertow.extensions;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import java.util.Objects;
-import javax.inject.Provider;
+import java.util.function.Supplier;
 
 /**
  * Delays initialization of a caller-supplied HTTP handler until a request is
  * received.
  */
 public final class LazyHandler implements HttpHandler {
-  private final Provider<? extends HttpHandler> handlerProvider;
+  private final Supplier<? extends HttpHandler> handlerSupplier;
 
-  public LazyHandler(Provider<? extends HttpHandler> handlerProvider) {
-    this.handlerProvider = Objects.requireNonNull(handlerProvider);
+  public LazyHandler(Supplier<? extends HttpHandler> handlerSupplier) {
+    this.handlerSupplier = Objects.requireNonNull(handlerSupplier);
   }
 
   @Override
   public void handleRequest(HttpServerExchange exchange) throws Exception {
-    HttpHandler handler = handlerProvider.get();
+    HttpHandler handler = handlerSupplier.get();
     handler.handleRequest(exchange);
   }
 }
