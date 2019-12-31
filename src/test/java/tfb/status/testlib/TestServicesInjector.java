b16d7515b453a5dd0632af4d6555c696f50cd794
Michael Hixson <mhixson@techempower.com>
2019-12-30 22:42:11 -0800

    Sort imports

--- src/test/java/tfb/status/testlib/TestServicesInjector.java
+++ src/test/java/tfb/status/testlib/TestServicesInjector.java
@@ -5,8 +5,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import tfb.status.hk2.extensions.Services;
 import tfb.status.bootstrap.ServicesBinder;
+import tfb.status.hk2.extensions.Services;
 
 /**
  * Allows instances of service classes to be injected into test methods as
