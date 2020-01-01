f34550004d17aff699f64cec4c25a2313fa46b9a
Michael Hixson <mhixson@techempower.com>
2020-01-01 11:12:01 -0800

    Convert HTTP handler path annotations to qualifiers

--- src/main/java/tfb/status/handler/ExactPath.java
+++ src/main/java/tfb/status/handler/ExactPath.java
@@ -6,12 +6,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.inject.Qualifier;
 import org.glassfish.hk2.api.Factory;
 
 /**
  * Indicates that an {@link HttpHandler} handles requests whose paths are
- * exactly equal to the specified string.  The {@link RootHandler} will forward
- * those requests to that handler.
+ * exactly equal to the specified string.  The {@link AllPaths} handler will
+ * forward those requests to that handler.
+ *
+ * <p>This handler must advertise the {@link HttpHandler} contract.
  *
  * <p>This annotation may be used on classes that implement {@link HttpHandler}
  * and on classes that implement {@link Factory Factory&lt;HttpHandler&gt;}.
@@ -21,11 +24,17 @@ import org.glassfish.hk2.api.Factory;
  * HttpServerExchange#getRelativePath() relative path} of the exchange will be
  * empty.
  *
- * @see RootHandler
  * @see PrefixPath
+ * @see AllPaths
  */
+@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({
+    ElementType.TYPE,
+    ElementType.METHOD,
+    ElementType.FIELD,
+    ElementType.PARAMETER
+})
 public @interface ExactPath {
   /**
    * The exact path string to be matched.
--- src/main/java/tfb/status/handler/PrefixPath.java
+++ src/main/java/tfb/status/handler/PrefixPath.java
@@ -6,12 +6,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.inject.Qualifier;
 import org.glassfish.hk2.api.Factory;
 
 /**
  * Indicates that an {@link HttpHandler} handles requests whose paths are
- * prefixed by the specified string.  The {@link RootHandler} will forward those
- * requests to that handler.
+ * prefixed by the specified string.  The {@link AllPaths} handler will forward
+ * those requests to that handler.
+ *
+ * <p>This handler must advertise the {@link HttpHandler} contract.
  *
  * <p>This annotation may be used on classes that implement {@link HttpHandler}
  * and on classes that implement {@link Factory Factory&lt;HttpHandler&gt;}.
@@ -22,11 +25,17 @@ import org.glassfish.hk2.api.Factory;
  * include the prefix of the {@linkplain HttpServerExchange#getRequestPath()
  * full request path} that was specified in this annotation.
  *
- * @see RootHandler
  * @see ExactPath
+ * @see AllPaths
  */
+@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({
+    ElementType.TYPE,
+    ElementType.METHOD,
+    ElementType.FIELD,
+    ElementType.PARAMETER
+})
 public @interface PrefixPath {
   /**
    * The path prefix string to be matched.
