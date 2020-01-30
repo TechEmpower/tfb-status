e9e22aded4ac6108cdb893b334a84cc6e96e67aa
smathews <smathews@techempower.com>
2020-01-30 14:43:49 -0800

    Ensure ShareResultsMailerTest the test is not run in parallel and add a TODO

--- src/test/java/tfb/status/service/ShareResultsMailerTest.java
+++ src/test/java/tfb/status/service/ShareResultsMailerTest.java
@@ -9,12 +9,15 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import tfb.status.testlib.MailServer;
 import tfb.status.testlib.TestServicesInjector;
 
 /**
  * Tests for {@link ShareResultsMailer}.
  */
+@Execution(ExecutionMode.SAME_THREAD) // currently not parallelizable
 @ExtendWith(TestServicesInjector.class)
 public final class ShareResultsMailerTest {
   /**
@@ -38,6 +41,9 @@ public final class ShareResultsMailerTest {
 
     messages = getShareDirectoryFullEmails(mailServer);
 
+    // TODO: Ensure an email was actually sent by this test. Otherwise, this is
+    //       not friendly to parallelism as another test could also cause an
+    //       email to be sent during this test.
     assertEquals(initialMessagesSize + 1, messages.size());
   }
 
