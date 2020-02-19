e18fd18718a4dcfcc195cd0a2b6ab64869c09842
Michael Hixson <mhixson@techempower.com>
2020-02-19 12:06:25 -0800

    Remove dependency on DefaultToUtf8Handler from AssetsHandler

--- src/main/java/tfb/status/handler/AssetsHandler.java
+++ src/main/java/tfb/status/handler/AssetsHandler.java
@@ -1,19 +1,24 @@
 package tfb.status.handler;
 
+import static com.google.common.net.MediaType.ANY_TEXT_TYPE;
 import static io.undertow.util.Methods.GET;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.net.MediaType;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.server.handlers.resource.PathResourceManager;
 import io.undertow.server.handlers.resource.ResourceHandler;
+import io.undertow.util.MimeMappings;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.Objects;
 import javax.inject.Singleton;
 import tfb.status.config.AssetsConfig;
 import tfb.status.handler.routing.PrefixPath;
 import tfb.status.hk2.extensions.Provides;
-import tfb.status.undertow.extensions.DefaultToUtf8Handler;
 import tfb.status.undertow.extensions.HttpHandlers;
 import tfb.status.undertow.extensions.MethodHandler;
 
@@ -37,7 +42,6 @@ public final class AssetsHandler {
 
     return HttpHandlers.chain(
         newResourceHandler(config, fileSystem),
-        handler -> new DefaultToUtf8Handler(handler),
         handler -> new MethodHandler().addMethod(GET, handler));
   }
 
@@ -50,12 +54,15 @@ public final class AssetsHandler {
       case CLASS_PATH: {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         var resourceManager = new ClassPathResourceManager(classLoader, "assets");
-        return new ResourceHandler(resourceManager);
+        var resourceHandler = new ResourceHandler(resourceManager);
+        resourceHandler.setMimeMappings(newMimeMappings());
+        return resourceHandler;
       }
       case FILE_SYSTEM: {
         Path assetsRoot = fileSystem.getPath("src/main/resources/assets");
         var resourceManager = new PathResourceManager(assetsRoot);
         var resourceHandler = new ResourceHandler(resourceManager);
+        resourceHandler.setMimeMappings(newMimeMappings());
         resourceHandler.setCacheTime(0);
         return resourceHandler;
       }
@@ -63,4 +70,36 @@ public final class AssetsHandler {
 
     throw new AssertionError("Unknown resource mode: " + config.mode);
   }
+
+  private static MimeMappings newMimeMappings() {
+    MimeMappings.Builder builder = MimeMappings.builder(false);
+
+    // Use the default mappings, but add "charset=utf-8" to all text types.
+    for (Map.Entry<String, String> entry
+        : MimeMappings.DEFAULT_MIME_MAPPINGS.entrySet()) {
+
+      MediaType mediaType = MediaType.parse(entry.getValue());
+      if (!mediaType.charset().isPresent() && isTextType(mediaType))
+        mediaType = mediaType.withCharset(UTF_8);
+
+      builder.addMapping(
+          /* extension= */ entry.getKey(),
+          /* contentType= */ mediaType.toString());
+    }
+
+    return builder.build();
+  }
+
+  private static boolean isTextType(MediaType mediaType) {
+    for (MediaType textType : KNOWN_TEXT_TYPES)
+      if (mediaType.is(textType))
+        return true;
+
+    return false;
+  }
+
+  private static final ImmutableSet<MediaType> KNOWN_TEXT_TYPES =
+      ImmutableSet.of(
+          ANY_TEXT_TYPE,
+          MediaType.create("application", "javascript"));
 }
