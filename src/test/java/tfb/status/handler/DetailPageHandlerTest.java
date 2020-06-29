a3f972e1a96a947cea2440fa6962e6087674f2d1
Michael Hixson <mhixson@techempower.com>
2020-06-29 13:34:18 -0700

    Add test for JSON view of results details

--- src/test/java/tfb/status/handler/DetailPageHandlerTest.java
+++ src/test/java/tfb/status/handler/DetailPageHandlerTest.java
@@ -2,18 +2,21 @@ package tfb.status.handler;
 
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static com.google.common.net.MediaType.HTML_UTF_8;
+import static com.google.common.net.MediaType.JSON_UTF_8;
 import static io.undertow.util.StatusCodes.NOT_FOUND;
 import static io.undertow.util.StatusCodes.OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static tfb.status.testlib.MoreAssertions.assertHtmlDocument;
 import static tfb.status.testlib.MoreAssertions.assertMediaType;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.http.HttpResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import tfb.status.testlib.HttpTester;
 import tfb.status.testlib.TestServicesInjector;
+import tfb.status.view.DetailPageView;
 
 /**
  * Tests for {@link DetailPageHandler}.
@@ -25,7 +28,7 @@ public final class DetailPageHandlerTest {
    * produces an HTML response.
    */
   @Test
-  public void testGet(HttpTester http)
+  public void testGetHtml(HttpTester http)
       throws IOException, InterruptedException {
 
     HttpResponse<String> response =
@@ -40,6 +43,33 @@ public final class DetailPageHandlerTest {
                 .orElse(null));
 
     assertHtmlDocument(response.body());
+  }
+
+  /**
+   * Verifies that a GET request for results detail JSON with a valid uuid
+   * produces a response with the expected content.
+   */
+  @Test
+  public void testGetJson(HttpTester http, ObjectMapper objectMapper)
+      throws IOException, InterruptedException {
+
+    HttpResponse<String> response =
+        http.getString("/results/598923fe-6491-41bd-a2b6-047f70860aed.json");
+
+    assertEquals(OK, response.statusCode());
+
+    assertMediaType(
+        JSON_UTF_8,
+        response.headers()
+                .firstValue(CONTENT_TYPE)
+                .orElse(null));
+
+    DetailPageView view =
+        objectMapper.readValue(
+            response.body(),
+            DetailPageView.class);
+
+    assertEquals("598923fe-6491-41bd-a2b6-047f70860aed", view.result.uuid);
   }
 
   /**
